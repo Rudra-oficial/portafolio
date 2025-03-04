a77f8239deec153795731786e93ded48c170c6e5
Ruben Rodriguez <rdrodriguez@estudiante.uniajc.edu.co>
2025-03-03 20:43:10 -0500

    Solución: Convertir page.js en cliente

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+"use client";  // ✅ Agregado aquí
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
